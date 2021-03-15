8a8dd2843769251a96ea198833aa8b7b4f1fb4e7
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-15 13:21:50 -0500

    Refactored Program.cs

--- Program.cs
+++ Program.cs
@@ -16,28 +16,19 @@ namespace Vehicles
                 fx, fxs, modelS
               };
 
-            Console.WriteLine("Electric Vehicles");
+            Console.WriteLine("=====Electric Vehicles=====");
 
-            // Show the current charge of the electric vehicles
+            // Iterates through all electric vehicles
+            // Show each vehicle's initial charge
+            // Recharge the vehicle
+            // Show the new charge
             foreach (IElectricVehicle ev in electricVehicles)
             {
                 ev.DisplayCharge();
-            }
-
-            // Recharge all electric vehicles
-            foreach (IElectricVehicle ev in electricVehicles)
-            {
-                // This should charge the vehicle to 100%
                 ev.ChargeBattery();
-            }
-
-            // Show the current charges again (Should be 100% now)
-            foreach (IElectricVehicle ev in electricVehicles)
-            {
                 ev.DisplayCharge();
+                Console.WriteLine();
             }
-
-            Console.WriteLine();
             /***********************************************/
 
             Ram ram = new Ram();
@@ -47,25 +38,18 @@ namespace Vehicles
                 ram, cessna150
               };
 
-            Console.WriteLine("Gas Vehicles");
+            Console.WriteLine("=====Gas Vehicles=====");
 
-            // Show the current gas level of the gas vehicles
+            // Iterates through all gas vehicles
+            // Show each vehicle's initial gas tank amount
+            // Refuel the vehicle
+            // Show the new gas tank
             foreach (IGasVehicle gv in gasVehicles)
             {
                 gv.DisplayGasTank();
-            }
-
-            // Refuel all gas vehicles
-            foreach (IGasVehicle gv in gasVehicles)
-            {
-                // This should completely refuel the gas tank
                 gv.RefuelTank();
-            }
-
-            // Show the current gas levels again (Should be 100%)
-            foreach (IGasVehicle gv in gasVehicles)
-            {
                 gv.DisplayGasTank();
+                Console.WriteLine();
             }
         }
     }
